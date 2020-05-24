9d56b332b196972b681a1f0579ecf1945368bfcd
PiyuPatil29 <piyunish91@gmail.com>
2020-05-24 13:51:40 +0530

    first commit

---
+++ XYZ/src/main/java/com/keywords/SimpleclassA.java
@@ -0,0 +1,11 @@
+package com.keywords;
+
+import org.testng.annotations.Test;
+
+public class SimpleclassA {
+	@Test
+	public void testInitial() {
+		System.out.println("I am testing Initial method");
+	}
+
+}
